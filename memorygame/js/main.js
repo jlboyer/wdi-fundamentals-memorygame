b4ce67f447eea8c9e4c7b5eb7b2b3af4460321a2
jlboyer <jlboyer1@gmail.com>
2019-05-30 11:26:14 -0500

    updated game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,4 +1,16 @@
-var cardOne = cardTwo = "queen"
-var cardThree = cardFour = "king"
+var cards = ["queen", "queen", "king", "king"]
+var cardOne = cards[0]
+var cardTwo = cards[2]
+var cardsInPlay = []
 
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
+
+if cardsInPlay.length === 2 {
+   if cardsInPlay[0] === cardsInPlay[1] {
+       alert("You found a match!")
+   } else if {
+       alert("Sorry, try again.")
+   }
+}
 console.log("User flipped" + cardThree)
