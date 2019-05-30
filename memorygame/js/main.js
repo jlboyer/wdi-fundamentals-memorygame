f356377de7c487972cd625ae4d7aa4ff24c8bbaf
jlboyer <jlboyer1@gmail.com>
2019-05-30 12:15:59 -0500

    match and flip functions

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,16 +1,22 @@
 var cards = ["queen", "queen", "king", "king"]
-var cardOne = cards[0]
-var cardTwo = cards[2]
 var cardsInPlay = []
 
-cardsInPlay.push(cardOne)
-cardsInPlay.push(cardTwo)
+function checkForMatch(){
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert("You found a match!")
+        } else {
+            alert("Sorry, try again.")
+        }
+    }
 
-if cardsInPlay.length === 2 {
-   if cardsInPlay[0] === cardsInPlay[1] {
-       alert("You found a match!")
-   } else if {
-       alert("Sorry, try again.")
-   }
+function flipCard(cardId){
+    console.log(`User flipped ${cards[cardId]}`)
+    cardsInPlay.push(cards[cardId])
+    if (cardsInPlay.length === 2) {
+    checkForMatch()
+    }
 }
-console.log("User flipped" + cardThree)
+
+flipCard(2)
+flipCard(3)
+
